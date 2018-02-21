70fac5b5ab754e34650accb2b7000100abc24f2b
Aditya Pratap Singh <adi.singh1992@gmail.com>
2018-02-21 23:06:58 +0530

    Update init.php

--- fb_oauth/init.php
+++ fb_oauth/init.php
@@ -6,15 +6,15 @@
 
 // Test Settings
 //    $fb = new Facebook\Facebook([
-//        'app_id' => '2009190809364895',
-//        'app_secret' => 'f925c7b5353090e8b6e92f0a92574b0d',
+//        'app_id' => '',
+//        'app_secret' => '',
 //        'default_graph_version' => 'v2.5',
 //        ]);
 
 // Live Settings
     $fb = new Facebook\Facebook([
-        'app_id' => '824413457683384',
-        'app_secret' => 'a83d1b9ff860d606f9b4f1b4f866bd12',
+        'app_id' => '',
+        'app_secret' => '',
         'default_graph_version' => 'v2.5',
         ]);
 
